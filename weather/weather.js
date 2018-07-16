1f21dbd751c7e9e6331c0b4fd3d0a90f89ed1998
Vibhav Kunj <romvibhav@gmail.com>
2018-07-17 03:22:55 +0530

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -5,7 +5,7 @@ const request = require('request');
 
 var getWeather = (lat,lng,callback) =>{
 request({
-    url: `https://api.darksky.net/forecast/77c6b7f797cb80cca2bf2c955fb9ea16/${lat},${lng}`,
+    url: `https://api.darksky.net/forecast/#YOUR_DARK_SKY_API_HERE/${lat},${lng}`,
     json:true
 },(error,response,body)=>{
     if (!error && response.statusCode === 200){
