f3b6af828e8587b71fe8d5f9c9f10ce400199e6a
Vibhav Kunj <romvibhav@gmail.com>
2018-07-17 03:22:13 +0530

    Update geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -4,7 +4,7 @@ var geocodeAddress = (address,callback) =>{
     var encodedAddress = encodeURIComponent(`${address}`);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDQUsCF43G19XwJdWPe2hPLEEETgK8kbbM`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=#YOUR_GOOGLE_GEOCODE_API_HERE`,
     json: true
 },(error,response,body)=>{
     if(error){
